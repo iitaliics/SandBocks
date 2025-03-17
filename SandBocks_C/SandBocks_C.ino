7a1306da672fe90bb9524cfd34b67de972fb4ebd
Sean McCabe <sean.mccabe@memjet.com>
2025-03-18 08:38:20 +1100

    The last commit didn't actually include my changes

--- SandBocks_C/SandBocks_C.ino
+++ SandBocks_C/SandBocks_C.ino
@@ -1,21 +1,58 @@
 #include <array>
+#include <random>
 
-int width = 32;
-int height = 16;
+
+
+/*
+TODO list:
+ - Finish the project rofl
+
+ - colour needs to be thought out, I need the colour_mode to be seperate so that it can be stacked for multiples in Cell class. The Colour
+ class should only really store the colour params. New base values will need to be acquired, and then new display values will need to be acquired. 
+ These cannot just be references, they need to be unique per instance.
+
+
+*/
+
+namespace RandomUtils {
+// Random engine and distribution seed
+static std::random_device rd;
+static std::mt19937 gen(rd());  // Mersenne Twister engine for random numbers
+
+// Generate a random byte  in the range [min, max]
+byte getRandomByte(byte min, byte max) {
+  if (min == max) {
+    return min;  // Prevent range issues
+  }
+  std::uniform_int_distribution<> dis(min, max - 1);
+  return dis(gen);
+}
+
+// Generate a random boolean with a given true probability (0.0 to 1.0)
+bool getRandomBool(float trueProbability = 0.5f) {
+  std::bernoulli_distribution dis(trueProbability);
+  return dis(gen);
+}
+};
+
+byte width = 32;
+byte height = 16;
 //Cell grid[width][height];
 
 class Grid {
 public:
-  int width;
-  int height;
+  byte width;
+  byte height;
   //    _ = ["Cell(air, temperature=23)" for _ in range(height)]
   //    self.grid = [_ for _ in range(width)]
 };
 
+
+
 struct coordinate_return {
   byte x;
   byte y;
-}
+};
 
 struct grid_return {
   byte x;
@@ -37,101 +74,111 @@ enum COLOURMODE {
   BLACKBODY
 };
 
+class Cell {
+public:
+  ELEMENT_ID element_id;
+  std::unique_ptr<Colour> colour_data;  // Smart pointer for automatic memory management
+  DIRECTION direction;
+  STATE_ID state_id;
+  bool flammable;
+  int temperature;
+  byte fuel;
+  byte life;
+  byte branches;
+  ELEMENT_ID clone_element;
+
+  Cell(ELEMENT_ID element_id, std::unique_ptr<Colour> colour_data, STATE_ID state_id, bool flammable, int temperature, byte fuel, byte life, byte branches, ELEMENT_ID clone_element_id)
+    : element(element_id), colour_data(std::move(colour_data)), direction(ELEMENT[element_id].default_direction), state(state_id), flammable(flammable), temperature(temperature), fuel(fuel), life(life), branches(branches), clone_element(clone_element_id) {
+    colour_data->set_base_colour();
+  }
+
+  // void update_colour() {
+  //   if COLOURMODE::NOISE
+  // }
+
+  // std::array<byte, 3> display_colour() {
+  //   int r =
+  //   return [r, g, b]
+  // }
+}
+
 class Colour {
 public:
-  byte param_red[2];
-  byte param_green[2];
-  byte param_blue[2];
+  const std::array<byte, 2> param_red;
+  const std::array<byte, 2> param_green;
+  const std::array<byte, 2> param_blue;
 
   byte base_red;
   byte base_green;
   byte base_blue;
 
-  byte display_red;
-  byte display_green;
-  byte display_blue;
-
   COLOURMODE colour_mode;
 
-  void set_base_colour(int param_r[2], int param_g[2], int param_b[2]) {
-    if (param_r[1] == 0) {
-      base_red = param_r[0];
-    } else {
-      base_red = random(param_r[0], param_r[1]);
-    }
-    if (param_g[1] == 0) {
-      base_green = param_g[0];
-    } else {
-      base_green = random(param_g[0], param_g[1]);
-    }
-    if (param_b[1] == 0) {
-      base_blue = param_b[0];
-    } else {
-      base_blue = random(param_b[0], param_b[1]);
-    }
-  };
+  Colour(std::array<byte, 2> param_red, std::array<byte, 2> param_green, std::array<byte, 2> param_blue, COLOURMODE colour_mode)
+    : param_red(param_red), param_green(param_green), param_blue(param_blue), colour_mode(colour_mode) {
+    set_base_colour(param_red, param_green, param_blue);
+  }
+
+  void set_base_colour() {
+    this->base_red = (param_red[1] == 0) ? param_red[0] : RandomUtils::getRandomByte(param_red[0], param_red[1]);
+    this->base_green = (param_green[1] == 0) ? param_green[0] : RandomUtils::getRandomByte(param_green[0], param_green[1]);
+    this->base_blue = (param_blue[1] == 0) ? param_blue[0] : RandomUtils::getRandomByte
+  }
 };
 
 class Particle {
 public:
-  String name;
-  Colour colour;
-  COLOURMODE colour_mode;
-  DIRECTION direction;
+  const String name;
+  const Colour colour_params;
+  const DIRECTION default_direction;
 
-  Particle(String name, Colour colour, COLOURMODE colour_mode = COLOURMODE::SOLID, DIRECTION direction = DIRECTION::NONE)
-    : name(name), colour(colour), colour_mode(colour_mode), direction(direction) {} //AI told me to do this :(
+  Particle(String name, Colour colour_params, COLOURMODE colour_mode = COLOURMODE::SOLID, DIRECTION direction = DIRECTION::NONE)
+    : name(name), colour_params(colour_params), colour_mode(colour_mode), direction(default_direction) {}
+
+  virtual ~Particle() {}  // Virtual destructor for polymorphism
 };
 
 class Updatable : public Particle {
 public:
-  int phase_change_temp[2];
-  Particle next_phase[2];
-  float thermal_conductivity;
-  // does name, colour and colour_mode need to be here if it's used 3 lines below?
+  const std::array<int, 2> phase_change_temp;
+  const std::array<ELEMENT_ID, 2> next_phase;
+  const float thermal_conductivity;
+  const DIRECTION default_direction = DIRECTION::NONE
 
-  Updatable(String name, Colour colour, COLOURMODE colour_mode, int phase_change_temp[2], Particle next_phase[2], float thermal_conductivity) {
-    : Particle(name, colour, colour_mode)  {
-      this->phase_change_temp[0] = phase_change_temp[0];
-      this->phase_change_temp[1] = phase_change_temp[1];
-      this->next_phase[0] = next_phase[0];
-      this->next_phase[1] = next_phase[1];
-    }
-  }
+  Updatable(String name, Colour colour_data, std::array<int, 2> phase_change_temp, std::array<ELEMENT_ID, 2> next_phase, float thermal_conductivity, DIRECTION default_direction)
+    : Particle(name, colour_data),
+                  phase_change_temp(phase_change_temp), next_phase(next_phase), thermal_conductivity(thermal_conductivity) {}
 
-  do(Grid grid, byte x, byte y) {
-    // Cell cell = grid.get(x, y);
-    // int current_temp = cell.temperature;
+  virtual ~Updatable() {}  // Virtual destructor for polymorphism
+
+  void do(Grid grid, byte x, byte y) {
+    Cell cell = grid.get(x, y);
+    int current_temp = cell.temperature;
     if (cell.element.next_phase[0] != ELEMENT_ID::NONE && current_temp < cell.element.phase_change_temp[0]) {
       cell.element = ELEMENT[ELEMENT_ID[cell.element.next_phase[0]]];
       cell.direction = cell.element.default_direction;
-      cell.update_colour_from_state_change()
+      cell.update_colour_from_state_change();
       return
     } else if (cell.element.next_phase[1] != ELEMENT_ID::NONE && current_temp > cell.element.phase_change_temp[1]) {
       cell.element = ELEMENT[ELEMENT_ID[cell.element.next_phase[1]]];
       cell.direction = cell.element.default_direction;
-      cell.update_colour_from_state_change()
+      cell.update_colour_from_state_change();
       return
     }
 
     if (cell.element.next_phase[1] == ELEMENT_ID::NONE && cell.flammable && current_temp > cell.element.phase_change_temp[1]) {
       cell.state = STATE::BURNING;
     }
-            
   }
 };
 
 class Solid : public Updatable {
-  public:
+public:
   float density;
   DIRECTION default_direction;
 
-  Solid(String name, Colour colour, COLOURMODE colour_mode, int phase_change_temp[2], Particle next_phase[2], float thermal_conductivity, float density, DIRECTION default_direction) {
-    : Updatable(name, colour, colour_mode, phase_change_temp, next_phase, thermal_conductivity) {
-      this->density = density;
-      this->default_direction = default_direction;
-    }
-  };
+  Solid(String name, Colour colour_data, COLOURMODE colour_mode, std::array<int, 2> phase_change_temp, std::array<ELEMENT_ID, 2> next_phase, float thermal_conductivity, float density, DIRECTION default_direction)
+    : Updatable(name, colour_data, phase_change_temp, next_phase, thermal_conductivity), density(density), default_direction(default_direction) {}
 
   move(Grid grid, byte x, byte y) {
     coordinate_return gaps[2] = grid.check_unimpeded(x, y);
@@ -139,65 +186,25 @@ class Solid : public Updatable {
 
     // Get length of valid entries
     byte length = 0;
-    for (auto i: gaps) {
-      if (gaps[i][0] == NULL) {
+    for (auto& gap : gaps) {
+      if (gap.x == NULL) {
         break;
       }
-      length ++;
+      length++;
     }
 
     if (length == 1) {
-      if (grid.can_swap_after_random(x, y, gaps[0][0], gaps[0][1])) {
-        grid.swap(x, y, gaps[0][0], gaps[0][1])
+      if (grid.can_swap_after_random(x, y, gaps[0].x, gaps[0].y)) {
+        grid.swap(x, y, gaps[0].x, gaps[0].y)
+      }
+    } else if (length == 2) {
+      byte index = RandomUtils::getRandomByte(0, length);
+      if (grid.can_swap_after_random(x, y, gaps[index].x, gaps[index].y)) {
+        grid.swap(x, y, gaps[index].x, gaps[index].y)
       }
     }
-    else if (length == 2) {
-      byte index = random(length);
-      if (grid.can_swap_after_random(x, y, gaps[index][0], gaps[index][1])) {
-        grid.swap(x, y, gaps[index][0], gaps[index][1])
-      }
-    }
   }
 }
-
-class Liquid : public Updatable {
-  public:
-  float density;
-  DIRECTION default_direction;
-
-  Liquid(String name, Colour colour, COLOURMODE colour_mode, int phase_change_temp[2], Particle next_phase[2], float thermal_conductivity, float density, DIRECTION default_direction) {
-    : Updatable(name, colour, colour_mode, phase_change_temp, next_phase, thermal_conductivity) {
-      this->density = density;
-      this->default_direction = default_direction;
-    }
-  };
-
-  move(Grid grid, byte x, byte y) {
-    coordinate_return gaps[3] = grid.check_unimpeded(x, y);
-    // TODO check unimpeded when the direct below space is free will only return an array of length 1. But now we have to explicitly set it to 3
-  }
-}
-
-class Gas : public Updatable {
-  public:
-  float density;
-  DIRECTION default_direction;
-
-  Gas(String name, Colour colour, COLOURMODE colour_mode, int phase_change_temp[2], Particle next_phase[2], float thermal_conductivity, float density, DIRECTION default_direction) {
-    : Updatable(name, colour, colour_mode, phase_change_temp, next_phase, thermal_conductivity) {
-      this->density = density;
-      this->default_direction = default_direction;
-    }
-  };
-
-  move(Grid grid, byte x, byte y) {
-    coordinate_return gaps[3] = grid.check_unimpeded(x, y);
-    // TODO check unimpeded when the direct below space is free will only return an array of length 1. But now we have to explicitly set it to 3
-  }
-}
-
-// Special Material Classes:
-
 
 enum ELEMENT_ID {
   AIR,
@@ -229,47 +236,34 @@ enum ELEMENT_ID {
   NONE
 }
 
+/*
+
+static const std::map<ELEMENT_ID, std::array<std::array<byte, 2>, 3>> element_params = {
+    {ELEMENT_ID::WATER, {{ {0, 50}, {100, 150}, {200, 255} }}},  // Red, Green, Blue
+    {ELEMENT_ID::AIR,   {{ {200, 255}, {200, 255}, {255, 255} }}},
+    // Add more elements...
+};
+
 Particle* ELEMENT[] = {
-  Gas(ELEMENT_ID::AIR, {{0, 0}, {0, 0}, {0, 0}}, {COLOURMODE::SOLID}, {NULL, NULL}, {ELEMENT_ID::NONE, ELEMENT_ID::NONE})
-// Gas("Air", (0, 0, 0), (colourMode["BLACKBODY"],), [None, None], [None, None], 0.026, 1.23, False),
-// Solid(, ([200, 250], [125, 200], [75, 100]), (colourMode['STATIC'], colourMode['BLACKBODY']), [None, None], [None, None], 0.4, 1602, True)
+    new Gas(
+        ELEMENT_ID::AIR,
+        element_params.at(ELEMENT_ID::AIR),     // Retrieve shared parameters for AIR
+        COLOURMODE::SOLID,
+        { ELEMENT_ID::NONE, ELEMENT_ID::NONE }
+    ),
+    new Gas(
+        ELEMENT_ID::WATER,
+        element_params.at(ELEMENT_ID::WATER),  // Retrieve shared parameters for WATER
+        COLOURMODE::SOLID,
+        { ELEMENT_ID::NONE, ELEMENT_ID::NONE }
+    ),
+    // Add more materials...
+};
 
-// water = Liquid("Water", (0, 0, 255), (colourMode["TEMPERATURE_DOWN"],), [0, 100], [None, None], 0.6, 997, True)
-// ice = Updatable("Ice", ([55, 85], [55, 85], 255), (colourMode["STATIC"],), [None, 0], [None, water], 2.22)
-// steam = Gas("Steam", (200, 200, 200), (colourMode["TEMPERATURE_DOWN"],), [100, None], [water, None], 0.0184, 0.6, False)
-// snow = Solid("Snow", ([240, 254], [240, 254], [240, 254]), (colourMode["STATIC"], ), [None, 0], [None, water], 0.05, 70, True)
-// water.next_phase = [ice, steam]
-// ice.next_phase = [None, water]
-// steam.next_phase = [water, None]
+*/
 
-// smoke = Gas("Smoke", (254, 254, 254), (colourMode["STATIC"],), [None, 300], [None, None], 0.6, 0.6, False)
-// fire = Gas("Fire", ([200, 254], [0, 50], [0, 50]), (colourMode["NOISE"],), [300, None], [smoke, None], 0.6, 0.6, False)
-// smoke.next_phase=[None, fire]
-
-// oil = Liquid("Oil", (50, 25, 25), (colourMode["SOLID"],), [None, 300], [None, None], 0.6, 800, True)
-
-// lava = Liquid("Lava", (254, [150, 250], [150, 250]), (colourMode['NOISE'],), [1160, None], [None, None], 2, 2400, True)
-// stone = Solid("Stone", (200, 200, [190, 210]), (colourMode['STATIC'], colourMode['BLACKBODY']), [None, 1160], [None, lava], 2.5, 2500, True)
-// lava.next_phase = [stone, None]
-// stone.next_phase = [None, lava]
-
-// wood = Updatable("Wood", ([50, 100], [25, 55], 0), (colourMode['STATIC'],), [None, 250], [None, None], 0.15)
-
-// molten_metal = Liquid("Molten Metal", (125, 75, 85), (colourMode['BLACKBODY'],), [1538, None], [None, None], 2.5, 7800, True)
-// metal = Updatable("Metal", (75, 75, 85), (colourMode['BLACKBODY'],), [None, 1538], [None, molten_metal], 2.5)
-// molten_metal.next_phase = [metal, None]
-// metal.next_phase = [None, molten_metal]
-
-// block = Particle("Block", (100, 100, 100), (colourMode["SOLID"], ))
-
-// # Special
-// destruct = Destruct("Destruct", ([25, 55], [25, 55], [25, 55]), (colourMode["NOISE"],),)
-// grow = Grow("Grow", (0, [150, 250], 10), (colourMode["STATIC"],), [None, 300], [None, None], 0.6)
-// clone = Clone("Clone", (150, 150, 0), (colourMode['SOLID'], ))
-// cold = Temperature("Cold", ([50, 75], [50, 75], [100, 254]), (colourMode['NOISE'],), [None, None], [None, None], 0.05, -10000)
-// hot = Temperature("Hot", ([100, 254], [50, 75], [50, 75]), (colourMode['NOISE'],), [None, None], [None, None], 0.05, 10000)
-// conduct = Updatable("Conduct", (100, 200, 100), (colourMode["BLACKBODY"], colourMode["TEMPERATURE_DOWN"],), [None, None], [None, None], 5)
-// flip = Flip("Flip", (25, 240, 0), (colourMode['SOLID'], ))
+Particle* ELEMENT[] = {
+  Gas(ELEMENT_ID::AIR, { { 0, 0 }, { 0, 0 }, { 0, 0 } }, { COLOURMODE::SOLID }, { NULL, NULL }, { ELEMENT_ID::NONE, ELEMENT_ID::NONE })
 };
 
 enum STATE_ID {
@@ -283,7 +277,8 @@ enum STATE {
   // DECAY = ,
 }
 
-void setup() {
+void
+setup() {
   // put your setup code here, to run once:
 }
 
